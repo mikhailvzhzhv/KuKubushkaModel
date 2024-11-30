f81c1348456c12a81fae43cb540d2e61b382fffa
mikhail <mihaelvozhzhov@gmail.com>
2024-11-30 20:18:23 +0700

    change gitignore

--- src/test/java/ru/nsu/vozhzhov/model/FileManagerSysTest.java
+++ src/test/java/ru/nsu/vozhzhov/model/FileManagerSysTest.java
@@ -9,7 +9,12 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 class FileManagerSysTest {
 
     private Path tempDir;
